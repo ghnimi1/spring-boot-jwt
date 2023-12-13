9c7f61c3a06768686791a7e367d1b2035aaa28f9
ghnimihassen <ghnimih@gmail.com>
2023-12-13 21:25:45 +0100

    update user model

---
+++ src/main/java/com/example/formationtcf/model/Test.java
@@ -0,0 +1,33 @@
+package com.example.formationtcf.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String text;
+    private String type;
+    private String question;
+    private String choix;
+    private String response;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<User> users = new HashSet<>();
+}
+
