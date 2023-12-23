6d13cb5836b5d87a9364431b77c3127476053e3d
ghnimihassen <ghnimih@gmail.com>
2023-12-23 18:59:30 +0100

    update test

--- src/main/java/com/example/formationtcf/controller/TestController.java
+++ src/main/java/com/example/formationtcf/controller/TestController.java
@@ -1,13 +1,11 @@
 package com.example.formationtcf.controller;
 
 import com.example.formationtcf.dto.TestDto;
-import com.example.formationtcf.model.Test;
 import com.example.formationtcf.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/tests")
@@ -23,7 +21,6 @@ public class TestController {
 
     @GetMapping("/{id}")
     public TestDto getTest(@PathVariable Long id) {
-        // Implement logic to convert entity to DTO and return
         return new TestDto();
     }
 }
